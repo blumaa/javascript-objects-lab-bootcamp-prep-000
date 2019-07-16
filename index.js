81c7ed9a46e7d3c9f5d51dcb892c33561ddb989f
Aaron Blum <blumaa@gmail.com>
2019-07-16 18:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return(recipe = Object.assign(recipes, key, value));
+  return Object.assign(recipes, key, value);
 }
